9c7dbe3c2c5a988c62655b4bc0d0291024d63722
vaibhav2740 <vaibhavsingh2740@gmail.com>
2025-06-18 02:40:47 +0530

    comments removed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,17 +1,3 @@
-// import React from 'react'
-// import './Footer.css'
-//
-// const Footer = () => {
-//   return (
-//     <div className='footer'>
-//       <p>Copyright @ 2024, Cryptoplace - All Right Reserved.</p>
-//     </div>
-//   )
-// }
-//
-// export default Footer
-
-import React from 'react'
 import './Footer.css'
 
 const Footer = () => {
