2da0b684cf917a8294cf212c07ba6dbadb688f4e
vaibhav2740 <vaibhavsingh2740@gmail.com>
2025-06-18 10:18:37 +0530

    coin page removed

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
-import Coin from "./pages/Coin/Coin";
+// import Coin from "./pages/Coin/Coin";
 
 const App: React.FC = () => {
     return (
@@ -10,7 +10,7 @@ const App: React.FC = () => {
             <Navbar />
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/coin/:coinId" element={<Coin />} />
+                {/*<Route path="/coin/:coinId" element={<Coin />} />*/}
             </Routes>
         </div>
     );
